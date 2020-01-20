c4440234918fc8e5aebd66f22d5ac57682013485
JaydaWatson <watsonjaydabooking@gmail.com>
2020-01-20 14:11:57 -0500

    First Website Upload

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,9 +1,28 @@
 <?php
+$name = $_Post['name'];
+$visitor_email = $_Post['email'];
+$message = $_Post['message'];
 
-if(isset($_POST['Send']))
-{
-    echo 'Working';
-}
+
+$email_from = 'Beat Box';
+
+$email_subject = "New Form Submission";
+
+$email_body = "User Name: $name.\n".
+"User Email: $visitor_email.\n".
+"User Message: $message.\n";
+
+
+$to = "watsonjaydabooking@gmail.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: Contact.html");
+
 
 
 ?>
